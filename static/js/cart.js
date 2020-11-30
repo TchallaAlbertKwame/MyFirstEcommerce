e01933e5b4492c57536cce14196ec482a1d7d403
TchallaAlbertKwame <68847248+TchallaAlbertKwame@users.noreply.github.com>
2020-11-30 10:37:27 +0000

    first commit

---
+++ static/js/cart.js
@@ -0,0 +1,72 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+    for (var i = 0; i < updateBtns.length; i++) {
+            updateBtns[i].addEventListener('click', function(){
+                var productId = this.dataset.product
+                var action = this.dataset.action
+                console.log('productId:', productId, 'Action:', action)
+
+                console.log('USER:', user)
+
+                if (user == 'AnonymousUser'){
+                    addCookieItem(productId, action)
+
+                }else{
+                     updateUserOrder(productId, action)
+                }
+
+            })
+    }
+
+ function addCookieItem(productId, action){
+    console.log('Not logged in...')
+//Adding items to the cart as a Guest user
+    if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+//Removing items from the cart as a Guest user
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Remove Item' )
+			delete cart[productId];
+		}
+	}
+
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
+
+}
+
+
+function updateUserOrder(productId, action){
+        console.log(' logged in, sending data...')
+        var url = '/update_item/'
+
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			},
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+
+		.then((data) => {
+		   console.log('data:',data)
+		    location.reload()
+		});
+
+
+
+}
